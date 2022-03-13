766abb103835c5ad6960774f151fd4c68bb0bfe0
yasintorun <yasintorun4680@gmail.com>
2022-03-13 12:50:52 +0300

    Created problem model

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import problemRouter from "./api/problem.route.js";
 import trackRouter from './api/track.route.js'
 import difficultyRouter from "./static/difficulty.route.js";
 const router = express.Router()
@@ -12,6 +13,7 @@ router.get("/", (req, res, next) => {
 
 router.use("/tracks", trackRouter)
 router.use("/difficulties", difficultyRouter)
+router.use("/problems", problemRouter)
 
 
 export default router
