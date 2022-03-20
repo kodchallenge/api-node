b0258c635dc91d28fbae453c12c22911c15fccde
yasintorun <yasintorun4680@gmail.com>
2022-03-20 17:13:40 +0300

    Complete login&signup transaction

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,41 @@
-const login = (req, res, next) => {
+import UserModel from "../models/user.model.js"
+import Result from '../utils/result.js'
+import jwt from 'jsonwebtoken'
+
+const maxAge = 60*60*24
+const createToken = (id) => {
+    return jwt.sign({id}, 'MySuperPassword!', {
+        expiresIn: maxAge
+    })
+}
+
+const login = async (req, res, next) => {
     const authData = req.body
+    try {
+        const user = await UserModel.login(authData.username, authData.password)
+        const token = createToken(user._id)
+        
+        Result.success(res, "Giriş Başarılı", token)
+    }catch(err) {
+        Result.error(res, err.message)
+    }
 }
 
 const logout = (req, res, next) => {
 
 }
 
-const signup = (req, res, next) => {
-
+const signup = async (req, res, next) => {
+    const userData = req.body
+    
+    const user = new UserModel(userData)
+    user.save()
+    .then((result) => {
+        Result.success(res, "Kayıt başarılı")
+    })
+    .catch(err => {
+        Result.error(res, "Kayıt başarısız: "+err)
+    })
 }
 
 
--- src/routes/api/auth.route.js
+++ src/routes/api/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { login, logout } from "../../controllers/auth.controller.js";
+import { login, logout, signup } from "../../controllers/auth.controller.js";
 
 const authRouter = express.Router()
 
-codeRouter.post("/login", login)
-codeRouter.post("/logout", logout)
-codeRouter.post("/signup", signup)
+authRouter.post("/login", login)
+authRouter.post("/logout", logout)
+authRouter.post("/signup", signup)
 
 export default authRouter
